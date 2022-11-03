0622f0bf27da2b14bda00548681f7524e4a721bd
Yuyao Song <48168412+YY-SONG0718@users.noreply.github.com>
2022-11-03 13:13:02 +0000

    devtools check

---
+++ R/global.R
@@ -0,0 +1,5 @@
+#' log global variables: column name in biomaRt query and markers
+#' @name globalvariables
+#' @importFrom utils globalVariables
+
+utils::globalVariables(c("namespace_1003", "name_1006", "go_linkage_type", "cluster", "avg_log2FC", "p_val_adj", "gene", 'placehold'))
